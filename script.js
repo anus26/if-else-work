0b47e3c663313ce30c2ec5edf42e135a3099da22
anas-raza~ <anusraza268@gmail.com>
2024-02-29 11:32:14 -0800

    index file created

---
+++ script.js
@@ -0,0 +1,35 @@
+// var num = 20;
+// var num = +prompt('enter your num');
+// if (20 <60) {
+//     console.log('to earing karo')
+// }else if(20>8){
+//     console.log('sadi pki')
+// }
+//  var Age = 35;
+// var  Age = +prompt('Enter you Age');
+// if ( Age  >= 23 ){
+//     console.log('sadi pki')
+// }
+// else if(Age >= 20){
+//     console.log('study kar')
+// }
+// else if( Age <= 15    ){
+//     console.log('pisa kama')
+// }
+// else{
+//     console.log('ok') 
+// }
+
+var salary = 350000;
+var salary = +prompt('Enter your salary');
+
+if (salary >=30000){
+    console.log('sadi pki')
+}
+else if(salary <= 20000){
+    console.log('bhai sccuses hasil kar')
+    
+}
+else{
+    console.log('study kar')
+}
